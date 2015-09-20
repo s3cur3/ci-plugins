2e75bcfa800e4b8a053195de3789ff03c35438a1
Tyler Young <tyler@x-plane.com>
2015-09-20 11:08:12 -0500

    chmod 777 all the things! (Fix for permissions issues on various hosts)

---
+++ ci-practice-areas-cpt/lib/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+if( !defined('CI_SLUG') ) define('CI_SLUG', 'conversion-insights');
+if( !defined('CI_THEME_PREFIX') ) define('CI_THEME_PREFIX', 'ci');
+if( !defined('CI_TEXT_DOMAIN') ) define('CI_TEXT_DOMAIN', CI_SLUG);
+
+if( !defined('CI_SIZE_LG') ) define("CI_SIZE_LG", CI_SLIDE_TYPE . '-fullscreen');
+if( !defined('CI_SIZE_MD') ) define("CI_SIZE_MD", CI_SLIDE_TYPE . '-contentwidth');
+
+if( !defined('CI_PRACTICE_AREA_TYPE') ) define("CI_PRACTICE_AREA_TYPE", "ci-practice-area");
